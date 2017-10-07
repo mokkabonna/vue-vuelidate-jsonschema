af06dd19113f6e932d49d6a538e64ba1819ddc65
Martin Hansen <martin@martinhansen.no>
2017-10-08 00:07:39 +0200

    Various validators implemented

--- src/index.js
+++ src/index.js
@@ -1,11 +1,170 @@
 'use strict'
+
+var reduce = require('lodash/reduce')
+var set = require('lodash/set')
+var isString = require('lodash/isString')
+var omit = require('lodash/omit')
+var validators = require('vuelidate/lib/validators')
+var vuelidate = require('vuelidate')
+
+function minValidator(min) {
+  return vuelidate.withParams({
+    type: 'minimum',
+    min: min
+  }, function(val) {
+    return !validators.required(val) || val >= min
+  })
+}
+
+function maxValidator(max) {
+  return vuelidate.withParams({
+    type: 'maximum',
+    max: max
+  }, function(val) {
+    return !validators.required(val) || val <= max
+  })
+}
+
+function patternValidator(pattern) {
+  return vuelidate.withParams({
+    type: 'pattern',
+    pattern: pattern
+  }, function(val) {
+    return isString(val) && pattern.test(val)
+  })
+}
+
+function oneOfValidator(choices) {
+  return vuelidate.withParams({
+    type: 'oneOf',
+    choices: choices
+  }, function(val) {
+    return !validators.required(val) || choices.indexOf(val) !== -1
+  })
+}
+
+function getDefaultValue(schema) {
+  if (schema.hasOwnProperty('default')) {
+    return schema.default
+  } else if (schema.type === 'string') {
+    return ''
+  } else if (schema.type === 'object') {
+    return {}
+  } else if (schema.type === 'array') {
+    return []
+  } else {
+    return null
+  }
+}
+
+function setProperties(base, schema) {
+  Object.keys(schema.properties).forEach(function(key) {
+    var innerSchema = schema.properties[key]
+    base[key] = getDefaultValue(innerSchema)
+    if (innerSchema.type === 'object' && innerSchema.properties) {
+      setProperties(base[key], innerSchema)
+    }
+  })
+}
+
+function createDataProperties(schemas) {
+  return reduce(schemas, function(all, schema, mountPoint) {
+
+    setProperties(all, schema)
+
+    return all
+  }, {})
+}
+
+function isRootSchema(schema) {
+  return schema.hasOwnProperty('type') && schema.type === 'object'
+}
+
+function normalizeDirectSchema(schema) {
+  return {
+    '.': schema
+  }
+}
+
+function getValidationRules(schema) {
+  return reduce(schema.properties, function(all, propertySchema, propKey) {
+
+    var validationObj = {}
+
+    if (schema.required && schema.required.indexOf(propKey) !== -1) {
+      validationObj.required = validators.requiredIf(function(val) {
+        return this[propKey] === undefined
+      })
+    }
+
+    if (propertySchema.hasOwnProperty('minLength')) {
+      validationObj.required = validators.required
+      validationObj.minLength = validators.minLength(propertySchema.minLength)
+    }
+
+    if (propertySchema.hasOwnProperty('maxLength')) {
+      validationObj.maxLength = validators.maxLength(propertySchema.maxLength)
+    }
+
+    if (propertySchema.hasOwnProperty('minimum')) {
+      validationObj.required = validators.required
+      validationObj.minimum = minValidator(propertySchema.minimum)
+    }
+
+    if (propertySchema.hasOwnProperty('maximum')) {
+      validationObj.maximum = maxValidator(propertySchema.maximum)
+    }
+
+    if (propertySchema.hasOwnProperty('pattern')) {
+      validationObj.pattern = patternValidator(new RegExp(propertySchema.pattern))
+    }
+
+    if (propertySchema.hasOwnProperty('enum')) {
+      validationObj.required = validators.required
+      validationObj.oneOf = oneOfValidator(propertySchema.enum)
+    }
+
+    all[propKey] = validationObj
+
+    return all
+  }, {})
+}
+
+function generateValidationSchema(schemas) {
+  var root = getValidationRules(schemas['.']) || {}
+
+  return reduce(omit(schemas, '.'), function(all, schema, mountPoint) {
+    set(all, mountPoint, getValidationRules(schema))
+    return all
+  }, root)
+}
+
 module.exports = {
   install: function(Vue, options) {
     options = options || {}
 
     Vue.mixin({
-      created() {
-        console.log(this.$options)
+      validations() {
+        if (this.schemas) {
+          return generateValidationSchema(this.schemas)
+        } else {
+          return {}
+        }
+      },
+      beforeCreate() {
+        if (!this.$options.schema) return
+        var normalized = isRootSchema(this.$options.schema) ? normalizeDirectSchema(this.$options.schema) : this.$options.schema
+        var data = createDataProperties(normalized)
+
+        // expose schemas normalized
+        Vue.util.defineReactive(this, 'schemas', normalized)
+
+        for (var prop in data) {
+          if (data.hasOwnProperty(prop)) {
+            Vue.util.defineReactive(this, prop, data[prop])
+          }
+        }
+
       }
     })
 
---
+++ test/specs/index.spec.js
@@ -0,0 +1,352 @@
+var chai = require('chai')
+var requireUncached = require('require-uncached');
+var expect = chai.expect
+var plugin = requireUncached('../../src')
+var Vue = requireUncached('vue')
+var Vuelidate = requireUncached('vuelidate')
+
+describe('plugin', function() {
+  it('installs ok', function() {
+    Vue.use(plugin)
+  })
+
+  describe('when installed', function() {
+    beforeEach(function() {
+      Vue.use(plugin)
+      Vue.use(Vuelidate)
+    })
+
+    it('adds properties to data object', function() {
+      var vm = new Vue({
+        data() {
+          return {
+            existing: 7
+          }
+        },
+        schema: {
+          '.': {
+            type: 'object',
+            properties: {
+              prop1: {
+                type: 'string'
+              },
+              prop2: {
+                type: 'number'
+              },
+              prop3: {
+                type: 'object'
+              },
+              prop4: {
+                type: 'array'
+              }
+            }
+          }
+        }
+      })
+
+      expect(vm.prop1).to.eql('')
+      expect(vm.prop2).to.eql(null)
+      expect(vm.prop3).to.eql({})
+      expect(vm.prop4).to.eql([])
+    })
+
+    it('supports default value from schema', function() {
+      var vm = new Vue({
+        schema: {
+          '.': {
+            type: 'object',
+            properties: {
+              prop1: {
+                type: 'string',
+                default: '123'
+              }
+            }
+          }
+        }
+      })
+
+      expect(vm.prop1).to.eql('123')
+    })
+
+    it('supports nesting', function() {
+      var vm = new Vue({
+        schema: {
+          '.': {
+            type: 'object',
+            properties: {
+              obj1: {
+                type: 'object',
+                properties: {
+                  prop1: {
+                    type: 'string'
+                  }
+                }
+              }
+            }
+          }
+        }
+      })
+
+      expect(vm.obj1).to.eql({
+        prop1: ''
+      })
+    })
+
+    it('supports schema directly', function() {
+      var vm = new Vue({
+        schema: {
+          type: 'object',
+          properties: {
+            str: {
+              type: 'string'
+            }
+          }
+        }
+      })
+
+      expect(vm.str).to.eql('')
+    })
+
+    describe('validation', function() {
+      describe('required', function() {
+        // required should not be added based on json schema required, it has not the same meaning
+        it('adds required validator to model, not allowing undefined', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'string'
+                }
+              },
+              required: ['str']
+            }
+          })
+
+          expect(vm.$v.str.$params.required.type).to.eql('requiredIf')
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(true)
+        })
+      })
+
+      describe('minLength', function() {
+        it('adds required and minLength validator', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'string',
+                  minLength: 3
+                }
+              }
+            }
+          })
+
+          expect(vm.$v.str.$params.required.type).to.eql('required')
+          expect(vm.$v.str.$params.minLength.type).to.eql('minLength')
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = null
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = ''
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = '12'
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = '123'
+          expect(vm.$v.str.$invalid).to.eql(false)
+        })
+      })
+
+      describe('maxLength', function() {
+        it('adds maxLength validator', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'string',
+                  maxLength: 3
+                }
+              }
+            }
+          })
+
+          expect(vm.$v.str.$params.maxLength.type).to.eql('maxLength')
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = null
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = '123'
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = '1234'
+          expect(vm.$v.str.$invalid).to.eql(true)
+        })
+      })
+
+      describe('maxLength', function() {
+        it('adds maxLength validator', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'number',
+                  maxLength: 3
+                }
+              }
+            }
+          })
+
+          expect(vm.$v.str.$params.maxLength.type).to.eql('maxLength')
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = null
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = '123'
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = '1234'
+          expect(vm.$v.str.$invalid).to.eql(true)
+        })
+      })
+
+      describe('minimum', function() {
+        it('adds minimum validator', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'number',
+                  minimum: 3
+                }
+              }
+            }
+          })
+
+          expect(vm.$v.str.$params.required.type).to.eql('required')
+          expect(vm.$v.str.$params.minimum.type).to.eql('minimum')
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = null
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = 0
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = 2
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = 3
+          expect(vm.$v.str.$invalid).to.eql(false)
+        })
+      })
+
+      describe('exclusiveMinimum', function() {
+        it('should be tested')
+      })
+
+      describe('maximum', function() {
+        it('adds maximum validator', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'number',
+                  maximum: 3
+                }
+              }
+            }
+          })
+
+          expect(vm.$v.str.$params.maximum.type).to.eql('maximum')
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = null
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = 0
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = 3
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = 4
+          expect(vm.$v.str.$invalid).to.eql(true)
+        })
+      })
+
+
+      describe('exclusiveMaximum', function() {
+        it('should be tested')
+      })
+
+      describe('pattern', function() {
+        it('adds pattern validator', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'string',
+                  pattern: '\\d+abc'
+                }
+              }
+            }
+          })
+
+          expect(vm.$v.str.$params.pattern.type).to.eql('pattern')
+          vm.str = ''
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = null
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = '3abc'
+          expect(vm.$v.str.$invalid).to.eql(false)
+        })
+      })
+
+      describe('enum', function() {
+        it('adds enum validator', function() {
+          var vm = new Vue({
+            mixins: [Vuelidate.validationMixin],
+            schema: {
+              type: 'object',
+              properties: {
+                str: {
+                  type: 'string',
+                  enum: ['valid1', 'valid2']
+                }
+              }
+            }
+          })
+
+          expect(vm.$v.str.$params.required.type).to.eql('required')
+          expect(vm.$v.str.$params.oneOf.type).to.eql('oneOf')
+          vm.str = ''
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = undefined
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = null
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = 'dfs'
+          expect(vm.$v.str.$invalid).to.eql(true)
+          vm.str = 'valid1'
+          expect(vm.$v.str.$invalid).to.eql(false)
+          vm.str = 'valid2'
+          expect(vm.$v.str.$invalid).to.eql(false)
+        })
+      })
+
+    })
+  })
+})
